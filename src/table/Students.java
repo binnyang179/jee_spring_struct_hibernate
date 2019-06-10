bb51d941e6c0fed1b0aa2ae55c89c767d3c0a1e0
crab179 <binyang3179@gmail.com>
2019-06-10 16:55:41 +0800

    Initial commit

---
+++ src/table/Students.java
@@ -0,0 +1,95 @@
+package table;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Students {
+    private Integer id;
+    private String uname;
+    private Byte gender;
+    private Integer age;
+    private Integer grade;
+    private String clazz;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "uname")
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    @Basic
+    @Column(name = "gender")
+    public Byte getGender() {
+        return gender;
+    }
+
+    public void setGender(Byte gender) {
+        this.gender = gender;
+    }
+
+    @Basic
+    @Column(name = "age")
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "grade")
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    @Basic
+    @Column(name = "class")
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Students students = (Students) o;
+        return Objects.equals(id, students.id) &&
+                Objects.equals(uname, students.uname) &&
+                Objects.equals(gender, students.gender) &&
+                Objects.equals(age, students.age) &&
+                Objects.equals(grade, students.grade) &&
+                Objects.equals(clazz, students.clazz);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uname, gender, age, grade, clazz);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: crab179
+  Date: 6/10/19
+  Time: 3:28 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>index</title>
+  </head>
+  <body>
+  hello,java ee
+  </body>
+</html>
